7eefbc74b19ae7cea037ac89de4d20a2433f3ec8
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-03-30 05:32:52 +0100

    Create building.js

---
+++ server/models/building.js
@@ -0,0 +1,29 @@
+// models/building.js
+module.exports = (sequelize, DataTypes) => {
+    const Building = sequelize.define('Building', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      type: {
+        type: DataTypes.ENUM('farm', 'barracks', 'market', 'workshop'),
+        allowNull: false
+      },
+      level: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1
+      },
+      attributes: {
+        type: DataTypes.JSON,
+        defaultValue: {}
+      }
+    }, {});
+  
+    Building.associate = models => {
+      // Each Building belongs to a Settlement.
+      Building.belongsTo(models.Settlement, { foreignKey: 'settlementId', as: 'settlement' });
+    };
+  
+    return Building;
+  };
+  
