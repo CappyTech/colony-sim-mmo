6cc70975c28aef558ab7553d4c66cb17335fafb5
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-03-30 05:32:55 +0100

    Create faction.js

---
+++ server/models/faction.js
@@ -0,0 +1,23 @@
+// models/faction.js
+module.exports = (sequelize, DataTypes) => {
+    const Faction = sequelize.define('Faction', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      description: DataTypes.TEXT,
+      // Additional fields like reputation or territory can be added here
+    }, {});
+  
+    Faction.associate = models => {
+      // A Faction has many Subjects as members.
+      Faction.hasMany(models.Subject, { foreignKey: 'factionId', as: 'members' });
+  
+      // Optionally, a Faction might have events.
+      Faction.hasMany(models.Event, { foreignKey: 'factionId', as: 'events' });
+    };
+  
+    return Faction;
+  };
+  
